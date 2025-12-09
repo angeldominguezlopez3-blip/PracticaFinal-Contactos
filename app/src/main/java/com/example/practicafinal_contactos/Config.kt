c4cb677b5b7ec8ed394b72a31c93dbb767ffed2f
angeldominguezlopez3-blip <angeldominguezlopez3@gmail.com>
2025-12-09 09:27:20 -0600

    Cambios de mejora en la vista

--- app/src/main/java/com/example/practicafinal_contactos/Config.kt
+++ app/src/main/java/com/example/practicafinal_contactos/Config.kt
@@ -1,8 +1,9 @@
 package com.example.practicafinal_contactos
 
 object Config {
-    const val URL = "http://192.168.1.71/gestioncontactos/" // Cambia por tu IP
+    const val URL = "http://192.168.107.104/gestioncontactos/" // Cambia por tu IP
     const val IMGBB_API_KEY = "c62b2cf5484724a2f9967b0f14b1f3b1"
     // URL de la API de ImgBB
     const val IMGBB_UPLOAD_URL = "https://api.imgbb.com/1/upload"
 }
+
